ac1e40bcc169ff3cf759bf4981da6862486d5675
Gökhan <gn.ozdemir@gmail.com>
2024-11-18 20:14:07 +0300

    katılanları selamlar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
+const initialArray = ["Didem", "Neslinur", "Gökcen", "Şeyda"]
+
 function App() {
 
   return (
-    <>
+    <div>
       selamlar
-    </>
+      {initialArray.map((item, index) => <p key={index}>{item}</p>)}
+    </div>
   )
 }
 
