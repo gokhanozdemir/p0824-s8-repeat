34a73e9c9f8715a260bb8f46feeec3c2b1317545
Gökhan <gn.ozdemir@gmail.com>
2024-11-20 21:36:18 +0300

    Login formunu taslaklar ve Form.css ekler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
-import { useHistory } from "react-router-dom";
-
+import { useHistory, Link } from "react-router-dom";
+import './Form.css'
 function Login() {
 	let history = useHistory()
 
@@ -8,10 +8,34 @@ function Login() {
 		history.push("/who-is-watching");
 	};
 
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+
 	return (
 		<>
-			<div>Hayali Login Formu</div>
-			<button className="primary-button" onClick={handleLogin}>Login</button>
+			<h1>Sign In</h1>
+			<form onSubmit={handleSubmit} className="flex column gap-s">
+				<div className="input-group">
+					<label htmlFor="emailormobile">Email or mobile number</label>
+					<input type="text" id="emailormobile" placeholder="your user info" />
+				</div>
+				<div className="input-group">
+					<label htmlFor="pass">Password</label>
+					<input type="password" id="pass" placeholder="your password" />
+				</div>
+				<div className="input-group">
+					<input type="checkbox" id="remember" name="rememberMe" />
+					<label htmlFor="remember">Remember me</label>
+				</div>
+				<button className="primary-button" type="submit">Login</button>
+				<button className="secondary-button" type="button">Use a Sign-In Code</button>
+				<Link to="/reset-password">Forgot Password?</Link>
+				<div>
+					New to Witflix? <Link to="/register">Sign up now</Link>
+				</div>
+			</form>
+
 		</>
 	)
 }
