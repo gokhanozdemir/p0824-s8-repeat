fbc660e70535ff23766b1be647e7fae08d5ce03b
Gökhan <gn.ozdemir@gmail.com>
2024-11-20 22:01:54 +0300

    controlledForm'a dönüştürür

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,14 @@
 import { useHistory, Link } from "react-router-dom";
 import './Form.css'
+import { useState } from "react";
 function Login() {
+	const initialForm = {
+		userInfo: 'gokhan',
+		passField: '123',
+		rememberMe: true,
+	};
 	let history = useHistory()
-
+	const [formData, setFormData] = useState(initialForm)
 	const handleLogin = () => {
 		// TODO: ileride bu kısma Axios ile auth eklenecek.
 		history.push("/who-is-watching");
@@ -12,20 +18,30 @@ function Login() {
 		e.preventDefault();
 	}
 
+	const handleChange = (e) => {
+		const { type, name, value, checked } = e.target;
+		console.log("type", type, "name", name, "value", value, "checked", checked);
+		const newFormData = { ...formData };
+
+		const valueNew = type === "checkbox" ? checked : value;
+		newFormData[name] = valueNew;
+		setFormData(newFormData);
+	}
+
 	return (
 		<>
 			<h1>Sign In</h1>
 			<form onSubmit={handleSubmit} className="flex column gap-s">
 				<div className="input-group">
 					<label htmlFor="emailormobile">Email or mobile number</label>
-					<input type="text" id="emailormobile" placeholder="your user info" />
+					<input onChange={handleChange} name="userInfo" value={formData.userInfo} type="text" id="emailormobile" placeholder="your user info" />
 				</div>
 				<div className="input-group">
 					<label htmlFor="pass">Password</label>
-					<input type="password" id="pass" placeholder="your password" />
+					<input onChange={handleChange} name="passField" value={formData.passField} type="password" id="pass" placeholder="your password" />
 				</div>
 				<div className="input-group">
-					<input type="checkbox" id="remember" name="rememberMe" />
+					<input onChange={handleChange} name="rememberMe" checked={formData.rememberMe} type="checkbox" id="remember" />
 					<label htmlFor="remember">Remember me</label>
 				</div>
 				<button className="primary-button" type="submit">Login</button>
